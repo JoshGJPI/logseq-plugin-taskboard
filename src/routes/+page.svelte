e1ed3da888b1e5bcce066c4c4158512f882bbc6d
Josh Gannon <JoshG@Johnsonpace.com>
2023-09-26 17:02:33 -0500

    setup initial files for test build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+    <h1>This is a title</h1>
+    <p>This is a test</p>
+</div>
